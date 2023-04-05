585f4578ac938cd22e54e95523d9a8060ad65422
db1703 <debernardifranco2003@gmail.com>
2023-04-05 17:40:27 -0300

    Log4j

--- src/main/java/com/example/clinicaOdontologica/ClinicaOdontologicaApplication.java
+++ src/main/java/com/example/clinicaOdontologica/ClinicaOdontologicaApplication.java
@@ -1,12 +1,18 @@
 package com.example.clinicaOdontologica;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.log4j.*;
 
 @SpringBootApplication
 public class ClinicaOdontologicaApplication {
 
+	private static final Logger logger = Logger.getLogger(ClinicaOdontologicaApplication.class);
+
 	public static void main(String[] args) {
+
+		PropertyConfigurator.configure("log4j.properties");
 		SpringApplication.run(ClinicaOdontologicaApplication.class, args);
 	}
 
